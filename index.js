e55727088b10231df3f5a9ca3d3ff0d38fa7b0a4
Anzal-F <anzalmohamed09@gmail.com>
2025-03-02 21:53:14 -0500

    Add the route to the index.js

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 import express from "express";
 import cors from "cors";
+import path from "path";
 import "dotenv/config";
 import tagsRoutes from "./routes/tags.js";
+import photosRoutes from "./routes/photos.js";
 
 const app = express();
 const port = process.env.PORT ?? 8080;
@@ -9,14 +11,16 @@ const port = process.env.PORT ?? 8080;
 app.use(cors());
 app.use(express.json());
 
-// Routes
-app.use("/tags", tagsRoutes);
 
-// Default route
+app.use("/images", express.static(path.resolve("public/images")));
+
+
+app.use("/tags", tagsRoutes);
+app.use("/photos", photosRoutes);
+
 app.get("/", (_req, res) => res.send("Hello Express!"));
 
-// Handles errors
+
 app.use((_req, res) => res.status(404).json({ message: "Route not found" }));
 
-// Start server
 app.listen(port, () => console.log(`Server running on port ${port}`));
